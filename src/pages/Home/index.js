7c0e08ad2e59f554930d222f9d48c13b3553b5bb
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-04-14 09:56:45 -0300

    add "loading" na pagina até a api carregar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,7 @@ import './home.css'
 
 function Home() {
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
         async function loadMovies() {
@@ -19,11 +20,21 @@ function Home() {
             })
 
             setFilmes(response.data.results.slice(0, 10));
+            setLoading(false)
         }
 
         loadMovies();
         
     }, [])
+
+
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes</h2>
+            </div>
+        )
+    }
 
     return(
         <div className="Container">
